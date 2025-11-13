14a6faee953ad82f7dd4d09424d0d476c0adb333
Alex Younger <thealexyounger@proton.me>
2025-11-12 21:50:58 -0600

    fix: correct error in wordmove doctor regarding rsync
    This change corrects an error in wordmove doctor
    'Wordmove::Doctor::Rsync#check!': undefined method '[]'
    for nil (NoMethodError).

--- lib/wordmove/doctor/rsync.rb
+++ lib/wordmove/doctor/rsync.rb
@@ -10,10 +10,13 @@ module Wordmove
       def check!
         logger.task "Checking rsync"
 
-        if (version = /\d\.\d.\d/.match(`rsync --version | head -n1`)[0])
-          logger.success "rsync is installed at version #{version}"
+        version_output = `rsync --version | head -n1 2>&1`
+
+        if (match = /\d+\.\d+\.\d+/.match(version_output))
+          logger.success "rsync is installed at version #{match[0]}"
         else
-          logger.error "rsync not found. And belive me: it's really strange it's not there."
+          logger.error "rsync not found or the version could not be detected. "\
+                       "Output was: #{version_output.strip}"
         end
       end
     end
